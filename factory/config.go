e0b5f21d927e6342301e5d81f5a415eb5802e3da
Fu Lian Weng <turtle11311@gmail.com>
2020-05-12 11:30:27 +0000

    feat(udm): merge config model

---
+++ factory/config.go
@@ -0,0 +1,68 @@
+/*
+ * UDM Configuration Factory
+ */
+
+package factory
+
+type Config struct {
+	Info *Info `yaml:"info"`
+
+	Configuration *Configuration `yaml:"configuration"`
+}
+
+type Info struct {
+	Version string `yaml:"version,omitempty"`
+
+	Description string `yaml:"description,omitempty"`
+}
+
+type Configuration struct {
+	UdmName string `yaml:"udmName,omitempty"`
+
+	Sbi *Sbi `yaml:"sbi,omitempty"`
+
+	ServiceNameList []string `yaml:"serviceNameList,omitempty"`
+
+	Udrclient *Udrclient `yaml:"udrclient,omitempty"`
+
+	Nrfclient *Nrfclient `yaml:"nrfclient,omitempty"`
+
+	NrfUri string `yaml:"nrfUri,omitempty"`
+
+	Keys *Keys `yaml:"keys,omitempty"`
+}
+
+type Sbi struct {
+	Scheme   string `yaml:"scheme"`
+	IPv4Addr string `yaml:"ipv4Addr,omitempty"`
+	// IPv6Addr string `yaml:"ipv6Addr,omitempty"`
+	Port int  `yaml:"port,omitempty"`
+	Tls  *Tls `yaml:"tls,omitempty"`
+}
+
+type Tls struct {
+	Log string `yaml:"log,omitempty"`
+
+	Pem string `yaml:"pem,omitempty"`
+
+	Key string `yaml:"key,omitempty"`
+}
+
+type Nrfclient struct {
+	Scheme   string `yaml:"scheme"`
+	Ipv4Addr string `yaml:"ipv4Addr,omitempty"`
+	Port     int    `yaml:"port,omitempty"`
+}
+
+type Udrclient struct {
+	Scheme   string `yaml:"scheme"`
+	Ipv4Addr string `yaml:"ipv4Addr,omitempty"`
+	Port     int    `yaml:"port,omitempty"`
+}
+
+type Keys struct {
+	UdmProfileAHNPrivateKey string `yaml:"udmProfileAHNPrivateKey,omitempty"`
+	UdmProfileAHNPublicKey  string `yaml:"udmProfileAHNPublicKey,omitempty"`
+	UdmProfileBHNPrivateKey string `yaml:"udmProfileBHNPrivateKey,omitempty"`
+	UdmProfileBHNPublicKey  string `yaml:"udmProfileBHNPublicKey,omitempty"`
+}
