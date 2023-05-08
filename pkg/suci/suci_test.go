9df336374201bd0a128d7bdf7dec83cffaf624b7
roy19991013 <80-ChienAn@users.noreply.gitlab.nems.cs.nctu.edu.tw>
2023-05-07 20:40:57 -0700

    change test case err type

--- pkg/suci/suci_test.go
+++ pkg/suci/suci_test.go
@@ -1,6 +1,9 @@
 package suci
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestToSupi(t *testing.T) {
 	suciProfiles := []SuciProfile{
@@ -19,36 +22,36 @@ func TestToSupi(t *testing.T) {
 	testCases := []struct {
 		suci         string
 		expectedSupi string
-		expectedErr  string
+		expectedErr  error
 	}{
 		{
 			suci:         "suci-0-208-93-0-0-0-00007487",
 			expectedSupi: "imsi-2089300007487",
-			expectedErr:  "",
+			expectedErr:  nil,
 		},
 		{
 			suci: "suci-0-208-93-0-1-1-b2e92f836055a255837debf850b528997ce0201cb82a" +
 				"dfe4be1f587d07d8457dcb02352410cddd9e730ef3fa87",
 			expectedSupi: "imsi-20893001002086",
-			expectedErr:  "",
+			expectedErr:  nil,
 		},
 		{
 			suci: "suci-0-208-93-0-2-2-039aab8376597021e855679a9778ea0b67396e68c66d" +
 				"f32c0f41e9acca2da9b9d146a33fc2716ac7dae96aa30a4d",
 			expectedSupi: "imsi-20893001002086",
-			expectedErr:  "",
+			expectedErr:  nil,
 		},
 		{
 			suci: "suci-0-208-93-0-2-2-0434a66778799d52fedd9326db4b690d092e05c9ba0ace5b413da" +
 				"fc0a40aa28ee00a79f790fa4da6a2ece892423adb130dc1b30e270b7d0088bdd716b93894891d5221a74c810d6b9350cc067c76",
 			expectedSupi: "",
-			expectedErr:  "crypto/elliptic: attempted operation on invalid point",
+			expectedErr:  fmt.Errorf("crypto/elliptic: attempted operation on invalid point"),
 		},
 	}
 	for i, tc := range testCases {
 		supi, err := ToSupi(tc.suci, suciProfiles)
 		if err != nil {
-			if err.Error() != tc.expectedErr {
+			if err.Error() != tc.expectedErr.Error() {
 				t.Errorf("TC%d fail: err[%s], expected[%s]\n", i, err, tc.expectedErr)
 			}
 		} else if supi != tc.expectedSupi {
