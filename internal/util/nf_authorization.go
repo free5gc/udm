a2e86c1c0d0475e99e5ce1cbd857473c58367ea5
CTFang@WireLab <ctfang.cs12@nycu.edu.tw>
2023-12-07 06:15:21 +0000

    Fix: remove unused code

--- internal/util/nf_authorization.go
+++ internal/util/nf_authorization.go
@@ -7,12 +7,11 @@ import (
 
 	"github.com/free5gc/openapi/oauth"
 	udm_context "github.com/free5gc/udm/internal/context"
-	"github.com/free5gc/udm/pkg/factory"
 )
 
 // This function would check the OAuth2 token, and the requestNF is in ServiceAllowNfType
 func AuthorizationCheck(c *gin.Context, serviceName string) error {
-	if factory.UdmConfig.GetOAuth() {
+	if udm_context.GetSelf().OAuth2Required {
 		oauth_err := oauth.VerifyOAuth(c.Request.Header.Get("Authorization"), serviceName,
 			udm_context.GetSelf().NrfCerPem)
 		if oauth_err != nil {
