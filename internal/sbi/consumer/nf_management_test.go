643102d221fbcc713f837218e7f08afab13dcb25
ianchen0119 <ychen.desl@gmail.com>
2024-01-03 02:08:35 +0000

    add UT for SendRegisterNFInstance()

---
+++ internal/sbi/consumer/nf_management_test.go
@@ -0,0 +1,24 @@
+package consumer
+
+import (
+	"testing"
+
+	"github.com/free5gc/openapi"
+	"github.com/free5gc/openapi/models"
+	"github.com/h2non/gock"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSendRegisterNFInstance(t *testing.T) {
+	defer gock.Off() // Flush pending mocks after test execution
+
+	gock.InterceptClient(openapi.GetHttpClient())
+	defer gock.RestoreClient(openapi.GetHttpClient())
+	gock.New("http://127.0.0.10:8000").
+		Put("/nnrf-nfm/v1/nf-instances/1").
+		Reply(200).
+		JSON(map[string]string{})
+
+	_, _, err := SendRegisterNFInstance("http://127.0.0.10:8000", "1", models.NfProfile{})
+	require.NoError(t, err)
+}
