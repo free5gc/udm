fe2687214d68fde3368822dd7f475529fcd41e19
CTFang@WireLab <ctfang.cs12@nycu.edu.tw>
2024-07-31 06:35:32 +0000

    fix: test and linter

--- internal/sbi/processor/generate_auth_data_test.go
+++ internal/sbi/processor/generate_auth_data_test.go
@@ -1,19 +1,20 @@
 package processor
 
 import (
+	"io"
 	"net/http/httptest"
 	"testing"
 
-	"github.com/free5gc/openapi"
-	"github.com/free5gc/openapi/models"
-	Nudr_DataRepository "github.com/free5gc/openapi/udr/DataRepository"
-	udm_context "github.com/free5gc/udm/internal/context"
-	"github.com/free5gc/udm/internal/sbi/consumer"
-	mockapp "github.com/free5gc/udm/pkg/mockapp"
 	"github.com/gin-gonic/gin"
 	"github.com/h2non/gock"
 	"github.com/stretchr/testify/require"
 	"go.uber.org/mock/gomock"
+
+	"github.com/free5gc/openapi"
+	"github.com/free5gc/openapi/models"
+	udm_context "github.com/free5gc/udm/internal/context"
+	"github.com/free5gc/udm/internal/sbi/consumer"
+	mockapp "github.com/free5gc/udm/pkg/mockapp"
 )
 
 func TestGenerateAuthDataProcedure(t *testing.T) {
@@ -22,17 +23,15 @@ func TestGenerateAuthDataProcedure(t *testing.T) {
 	gock.InterceptClient(openapi.GetHttpClient())
 	defer gock.RestoreClient(openapi.GetHttpClient())
 
-	queryRes := Nudr_DataRepository.QueryAuthSubsDataResponse{
-		AuthenticationSubscription: models.AuthenticationSubscription{
-			AuthenticationMethod:          models.AuthMethod__5_G_AKA,
-			EncPermanentKey:               "8baf473f2f8fd09487cccbd7097c6862",
-			ProtectionParameterId:         "8baf473f2f8fd09487cccbd7097c6862",
-			SequenceNumber:                &models.SequenceNumber{Sqn: "000000000023"},
-			AuthenticationManagementField: "8000",
-			AlgorithmId:                   "128-EEA0",
-			EncOpcKey:                     "8e27b6af0e692e750f32667a3b14605d",
-			EncTopcKey:                    "8e27b6",
-		},
+	queryRes := models.AuthenticationSubscription{
+		AuthenticationMethod:          models.AuthMethod__5_G_AKA,
+		EncPermanentKey:               "8baf473f2f8fd09487cccbd7097c6862",
+		ProtectionParameterId:         "8baf473f2f8fd09487cccbd7097c6862",
+		SequenceNumber:                &models.SequenceNumber{Sqn: "000000000023"},
+		AuthenticationManagementField: "8000",
+		AlgorithmId:                   "128-EEA0",
+		EncOpcKey:                     "8e27b6af0e692e750f32667a3b14605d",
+		EncTopcKey:                    "8e27b6",
 	}
 
 	gock.New("http://127.0.0.4:8000/nudr-dr/v2").
@@ -75,4 +74,39 @@ func TestGenerateAuthDataProcedure(t *testing.T) {
 	httpRecorder := httptest.NewRecorder()
 	c, _ := gin.CreateTestContext(httpRecorder)
 	testProcessor.GenerateAuthDataProcedure(c, authInfoReq, "imsi-208930000000001")
+
+	httpResp := httpRecorder.Result()
+	if errClose := httpResp.Body.Close(); errClose != nil {
+		t.Fatalf("Failed to close response body: %+v", errClose)
+	}
+
+	rawBytes, errReadAll := io.ReadAll(httpResp.Body)
+	if errReadAll != nil {
+		t.Fatalf("Failed to read response body: %+v", errReadAll)
+	}
+
+	var res models.AuthenticationInfoResult
+	err = openapi.Deserialize(&res, rawBytes, httpResp.Header.Get("Content-Type"))
+	if err != nil {
+		t.Fatalf("Failed to deserialize response body: %+v", err)
+	}
+
+	expectResponse := models.AuthenticationInfoResult{
+		AuthType: "5G_AKA",
+		AuthenticationVector: &models.AuthenticationVector{
+			AvType: "5G_HE_AKA",
+			// Rand:     "6823f0dc9da02a61f6224d278a6f65b0",
+			// Autn:     "1a7692538cf2800082662b9daa0396c5",
+			// XresStar: "10e6e9e7d4ed291e7b2dc81e41f1da17",
+			// Kausf:    "b0066a373555e6cd3efabfd14dfbc0bd91f5792445e2d80748daeb0a629d9b09",
+		},
+		Supi: "imsi-208930000000001",
+	}
+
+	require.Equal(t, 200, httpResp.StatusCode)
+
+	// Since GenerateAuthData have randomness, only check fix value
+	require.Equal(t, expectResponse.AuthType, res.AuthType)
+	require.Equal(t, expectResponse.Supi, res.Supi)
+	require.Equal(t, expectResponse.AuthenticationVector.AvType, res.AuthenticationVector.AvType)
 }
