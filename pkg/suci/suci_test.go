422f93b3f4a172aaad70f1003ae000169c9c6162
ubuntu <hjkja0511@gmail.com>
2024-10-18 13:31:52 +0000

    fix: lint error

--- pkg/suci/suci_test.go
+++ pkg/suci/suci_test.go
@@ -48,7 +48,8 @@ func TestToSupi(t *testing.T) {
 			expectedErr:  fmt.Errorf("crypto/elliptic: attempted operation on invalid point"),
 		},
 		{
-			suci:         "suci-0-001-01-0-2-2-03a7b1db2a9db9d44112b59d03d8243dc6089fd91d2ecb78f5d16298634682e94373888b22bdc9293d1681922e17",
+			suci: "suci-0-001-01-0-2-2-03a7b1db2a9db9d44112b59d03d8243dc6089fd91d2ecb" +
+				"78f5d16298634682e94373888b22bdc9293d1681922e17",
 			expectedSupi: "imsi-001010123456789",
 			expectedErr:  nil,
 		},
