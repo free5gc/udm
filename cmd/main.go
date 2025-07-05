f10a5dc70079c1867e4a1feb4180b469ddf11ba3
Tsung-Han Ho (Miles Ho) <mystes3016@gmail.com>
2025-07-05 22:42:31 +0800

    chore: update urfave/cli to v2
    Signed-off-by: Tsung-Han Ho (Miles Ho) <mystes3016@gmail.com>

--- cmd/main.go
+++ cmd/main.go
@@ -8,7 +8,7 @@ import (
 	"runtime/debug"
 	"syscall"
 
-	"github.com/urfave/cli"
+	"github.com/urfave/cli/v2"
 
 	"github.com/free5gc/udm/internal/logger"
 	"github.com/free5gc/udm/pkg/factory"
@@ -30,13 +30,15 @@ func main() {
 	app.Usage = "5G Unified Data Management (UDM)"
 	app.Action = action
 	app.Flags = []cli.Flag{
-		cli.StringFlag{
-			Name:  "config, c",
-			Usage: "Load configuration from `FILE`",
+		&cli.StringFlag{
+			Name:    "config",
+			Aliases: []string{"c"},
+			Usage:   "Load configuration from `FILE`",
 		},
-		cli.StringSliceFlag{
-			Name:  "log, l",
-			Usage: "Output NF log to `FILE`",
+		&cli.StringSliceFlag{
+			Name:    "log",
+			Aliases: []string{"l"},
+			Usage:   "Output NF log to `FILE`",
 		},
 	}
 
