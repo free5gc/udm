ee9e07daa1276707b749283eac0757bff7c7f54f
chchen7 <chchen.cs14@nycu.edu.tw>
2025-12-28 20:26:41 +0800

    feat: add more information

--- internal/util/supi.go
+++ internal/util/supi.go
@@ -5,11 +5,14 @@ import (
 	"strings"
 )
 
-// TS 29.571 & TS 23.003 SUPI format validation
+// TS 29.571 5.3.2 & TS 23.003
+// SUPI format validation
 var supiImsiRegex = regexp.MustCompile(`^imsi-[0-9]{5,15}$`)
-// TS 29.571 & TS 23.003 NAI format validation
+// TS 29.571 5.3.2 & TS 23.003 28.7.2 
+// NAI format validation
 var supiNaiRegex = regexp.MustCompile(`^nai-.+@.+$`)
-// TS 29.571 GCI/GLI format validation
+// TS 29.571 5.3.2 & TS 23.003 28.15.2(gci) & TS 23.003 28.16.2(gli)
+// GCI/GLI format validation
 var supiGciGliRegex = regexp.MustCompile(`^(gci|gli)-.+$`)
 
 // IsValidSupi checks if the given SUPI is valid according to 3GPP specifications
